0f221046eaf7a04cc9c3a249db20a80f19979537
letov7482-ui <letov7482@gmail.com>
2026-07-27 09:26:53 +0300

    Create ModuleManager.java

---
+++ src/main/java/net/midpoint/visuals/module/ModuleManager.java
@@ -0,0 +1,21 @@
+package net.midpoint.visuals.module;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModuleManager {
+    private static final Map<Class<? extends Module>, Module> modules = new HashMap<>();
+
+    public static void init() {
+        register(new BlockOverlayModule());
+    }
+
+    private static void register(Module module) {
+        modules.put(module.getClass(), module);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Module> T get(Class<T> clazz) {
+        return (T) modules.get(clazz);
+    }
+}
